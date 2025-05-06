a45498505560034f04792e195c8896c5f9c31fed
Mahdi <mahdiheidary636@gmail.com>
2025-05-06 21:29:48 +0330

    message

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -116,9 +116,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- zarinpal/urls.py
+++ zarinpal/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/payment/', views.payment_view, name='payment'),
-    path('/verify/', views.verify_view, name='verify'),
+    path('payment', views.payment_view, name='payment'),
+    path('verify', views.verify_view, name='verify'),
 ]
